f285a70d324ba214d0fd97fa254dd33f7b5b5098
piggy2021 <82191047+piggy2021@users.noreply.github.com>
2021-04-09 10:14:20 +0800

    first commit

---
+++ backbone/resnet101.py
@@ -0,0 +1,27 @@
+import torch
+from torch import nn
+import torchvision.models as models
+
+class ResNet101(nn.Module):
+    def __init__(self):
+        super(ResNet101, self).__init__()
+        net = models.resnet101(pretrained=True)
+        # net.load_state_dict(torch.load(resnext101_32_path))
+
+        net = list(net.children())
+        self.layer0 = nn.Sequential(*net[:4])
+        self.layer1 = net[4]
+        self.layer2 = net[5]
+        self.layer3 = net[6]
+        self.layer4 = net[7]
+
+    def forward(self, x):
+        layer0 = self.layer0(x)
+        layer1 = self.layer1(layer0)
+        layer2 = self.layer2(layer1)
+        layer3 = self.layer3(layer2)
+        layer4 = self.layer4(layer3)
+        return layer4
+
+if __name__ == '__main__':
+    model = ResNet101()
