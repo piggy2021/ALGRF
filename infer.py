3ce73201d0c1803d033ff93fc9d71afe679c5011
piggy2021 <82191047+piggy2021@users.noreply.github.com>
2021-04-09 15:19:31 +0800

    Add files via upload

--- infer.py
+++ infer.py
@@ -8,11 +8,8 @@ from torchvision import transforms
 
 from config import ecssd_path, hkuis_path, pascals_path, sod_path, dutomron_path, \
     davis_path, fbms_path, mcl_path, uvsd_path, visal_path, vos_path, segtrack_path, davsod_path
-from misc import check_mkdir, AvgMeter, cal_precision_recall_mae, cal_fmeasure
-# from models.net import SNet
-from models.net_i import INet
-from models.net_i101 import INet101
-from utils.utils_mine import load_part_of_model2, MaxMinNormalization
+from models.net import INet
+from utils_mine import MaxMinNormalization, check_mkdir, AvgMeter, cal_precision_recall_mae, cal_fmeasure
 import time
 from matplotlib import pyplot as plt
 
@@ -32,10 +29,8 @@ exp_name = 'VideoSaliency_2021-04-06 23:20:10'
 args = {
     'gnn': True,
     'snapshot': '184000',  # your snapshot filename (exclude extension name)
-    'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
-    'input_size': (380, 380),
-    'start': 0
+    'input_size': (380, 380)
 }
 
 img_transform = transforms.Compose([
@@ -80,20 +75,16 @@ imgs_path = os.path.join(visal_path, 'ViSal_test_single.txt')
 # gt_root = os.path.join(mcl_path, 'GT')
 # imgs_path = os.path.join(mcl_path, 'MCL_test_single.txt')
 
-def main(snapshot):
+def main():
     # net = R3Net(motion='', se_layer=False, dilation=False, basic_model='resnet50')
 
     net = INet(cfg=None, GNN=args['gnn'])
-    if snapshot is None:
-        print ('load snapshot \'%s\' for testing' % args['snapshot'])
-        # net.load_state_dict(torch.load('pretrained/R2Net.pth', map_location='cuda:2'))
-        # net = load_part_of_model2(net, 'pretrained/R2Net.pth', device_id=2)
-        net.load_state_dict(torch.load(os.path.join(ckpt_path, exp_name, args['snapshot'] + '.pth'),
-                                   map_location='cuda:' + str(device_id)))
-    else:
-        print('load snapshot \'%s\' for testing' % snapshot)
-        net.load_state_dict(torch.load(os.path.join(ckpt_path, exp_name, snapshot + '.pth'),
-                                       map_location='cuda:' + str(device_id)))
+
+    print ('load snapshot \'%s\' for testing' % args['snapshot'])
+    # net.load_state_dict(torch.load('pretrained/R2Net.pth', map_location='cuda:2'))
+    # net = load_part_of_model2(net, 'pretrained/R2Net.pth', device_id=2)
+    net.load_state_dict(torch.load(os.path.join(ckpt_path, exp_name, args['snapshot'] + '.pth'),
+                               map_location='cuda:' + str(device_id)))
     net.eval()
     net.cuda()
     results = {}
@@ -207,235 +198,6 @@ def main(snapshot):
 
     print ('test results:')
     print (results)
-    log_path = os.path.join('result_all.txt')
-    if snapshot is None:
-        open(log_path, 'a').write(exp_name + ' ' + args['snapshot'] + '\n')
-    else:
-        open(log_path, 'a').write(exp_name + ' ' + snapshot + '\n')
-    open(log_path, 'a').write(str(results) + '\n\n')
-
-
 if __name__ == '__main__':
-    if args['start'] > 0:
-        for i in range(args['start'], 204000, 4000):
-            main(str(i))
-    else:
-        main(None)
+    main()
 
-
-# BASNet
-# {'davis': {'fmeasure': 0.8566843568615541, 'mae': 0.032508174857025014}} 120000
-# {'davis': {'fmeasure': 0.8257118647276728, 'mae': 0.08186260435816704}} 100000
-# {'davis': {'fmeasure': 0.8162878069424612, 'mae': 0.03628622811730323}} 0
-# BASNet2
-# {'davis': {'fmeasure': 0.8249547930038379, 'mae': 0.06307644139293314}} 60000
-# {'davis': {'fmeasure': 0.8466711413681038, 'mae': 0.03978594446420074}} 100000
-# {'davis': {'fmeasure': 0.8372888555262563, 'mae': 0.03733236218858492}} 120000
-
-# gate module insert
-# {'davis': {'fmeasure': 0.8564294782400845, 'mae': 0.034741382499633856}} 120000
-# {'davis': {'fmeasure': 0.8367139657442704, 'mae': 0.039574989092051545}} 40000
-
-# R3Net (no distillation) VideoSaliency_2020-07-21 13:01:22
-# {'davis': {'fmeasure': 0.8562027006431742, 'mae': 0.02722195845054729}} 120000
-# {'davis': {'fmeasure': 0.8590793367156063, 'mae': 0.026678991545746208}} 100000
-# {'DAVSOD': {'fmeasure': 0.6050681244757543, 'mae': 0.08474575046947992}} 100000
-# {'VOS': {'fmeasure': 0.7547719681506946, 'mae': 0.0671947038013288}} 100000
-
-# R3Net (with distillation) VideoSaliency_2020-07-23 01:32:38
-# {'davis': {'fmeasure': 0.8708063007796304, 'mae': 0.030973868481739456}} 120000
-# {'davis': {'fmeasure': 0.8668223696599545, 'mae': 0.03256905773830208}} 100000
-# {'davis': {'fmeasure': 0.7926891841311283, 'mae': 0.05570007050745713}} 0
-
-# {'VOS': {'fmeasure': 0.7804980085734903, 'mae': 0.07218745285743372}} 120000
-
-# gate module insert VideoSaliency_2020-08-24 16:19:00
-# {'davis': {'fmeasure': 0.8770755494856973, 'mae': 0.03041000501182174}} 10000
-# {'davis': {'fmeasure': 0.878549663716236, 'mae': 0.028963380842353136}} 30000
-# {'VOS': {'fmeasure': 0.7901018306975077, 'mae': 0.06622779220610804}} 30000
-# {'DAVSOD': {'fmeasure': 0.6739642805392635, 'mae': 0.07081303084017548}} 30000
-
-# CPD
-# {'davis': {'fmeasure': 0.8699013545842154, 'mae': 0.03606130829488207}} 120000
-# {'davis': {'fmeasure': 0.8719889406088578, 'mae': 0.03487299259319628}} 100000
-# {'davis': {'fmeasure': 0.8695780875313027, 'mae': 0.03390185741145803}} 80000
-# {'davis': {'fmeasure': 0.800913855008486, 'mae': 0.0404164133863452}} 0
-
-# CPD2
-# {'davis': {'fmeasure': 0.8387031680456861, 'mae': 0.03758357432994166}}
-
-# CPD2 only finetune VideoSaliency_2020-08-31 02:01:21
-# {'davis': {'fmeasure': 0.8222878832873939, 'mae': 0.041747482896966434}} 60000
-# {'davis': {'fmeasure': 0.8385933219114949, 'mae': 0.03462121725596459}} 100000
-# {'davis': {'fmeasure': 0.844497796554646, 'mae': 0.03466052830857213}} 80000
-# {'davis': {'fmeasure': 0.8437427660140131, 'mae': 0.03307520743178606}} 120000
-
-# {'VOS': {'fmeasure': 0.7569595625226427, 'mae': 0.08371791356920238}} 80000
-# {'DAVSOD': {'fmeasure': 0.617840800123522, 'mae': 0.079172990510061}} 80000
-
-# {'VOS': {'fmeasure': 0.7405037052808612, 'mae': 0.07940268278758458}} 60000
-
-# {'DAVSOD': {'fmeasure': 0.6089755250989918, 'mae': 0.0791951545988771}} 100000
-# {'VOS': {'fmeasure': 0.7527223544711551, 'mae': 0.08485241885481223}} 100000
-
-# gate module insert VideoSaliency_2020-08-31 11:02:05
-# {'davis': {'fmeasure': 0.8546145606079505, 'mae': 0.03725592245174211}}
-# {'davis': {'fmeasure': 0.8545848500756481, 'mae': 0.036076494497675145}} 100000
-# {'davis': {'fmeasure': 0.8702094216216789, 'mae': 0.03227596775720227}} 8000
-# {'davis': {'fmeasure': 0.8721395434349012, 'mae': 0.0326152605046578}} 10000
-
-# {'DAVSOD': {'fmeasure': 0.6246623516711902, 'mae': 0.08521686317336055}} 10000
-# {'VOS': {'fmeasure': 0.7891096714831739, 'mae': 0.08420496569470534}} 10000
-
-# DSS
-# {'davis': {'fmeasure': 0.7765826462881504, 'mae': 0.04943853521917727}}
-
-# poolnet
-# {'davis': {'fmeasure': 0.8641983517948094, 'mae': 0.031427647911369386}} 120000
-# {'davis': {'fmeasure': 0.8643076508368502, 'mae': 0.03021199209724235}} 100000
-# {'davis': {'fmeasure': 0.8652781719534995, 'mae': 0.030149780373771085}} 80000
-# {'davis': {'fmeasure': 0.8629402103793395, 'mae': 0.030353318219999918}} 60000
-# {'davis': {'fmeasure': 0.8634947641279994, 'mae': 0.030284757482625123}} 40000
-# {'davis': {'fmeasure': 0.8604904754370101, 'mae': 0.030785979459017395}} 20000
-
-# poolnet only finetune
-# {'davis': {'fmeasure': 0.8567896379616566, 'mae': 0.028652147056242798}} 10000
-# {'davis': {'fmeasure': 0.8530932574012062, 'mae': 0.028328569737440374}} 20000
-# {'davis': {'fmeasure': 0.8573124361232648, 'mae': 0.02838192739825468}} 30000
-
-# {'VOS': {'fmeasure': 0.7404455357165366, 'mae': 0.07203330270967374}} 20000
-# {'DAVSOD': {'fmeasure': 0.6152740410451761, 'mae': 0.0780330552217813}} 20000
-
-# gate
-# {'davis': {'fmeasure': 0.8632113552777682, 'mae': 0.029550991612412567}} 40000
-# {'davis': {'fmeasure': 0.8634872265430311, 'mae': 0.029076951244899493}} 30000
-
-# {'VOS': {'fmeasure': 0.7589025935271624, 'mae': 0.07688885680114}} 30000
-# {'DAVSOD': {'fmeasure': 0.6359492324304611, 'mae': 0.08098192376104135}}
-
-# RAS
-
-# {'davis': {'fmeasure': 0.8644004769923315, 'mae': 0.03054644717665153}} 80000 + 40000
-# {'davis': {'fmeasure': 0.8635395432544891, 'mae': 0.030182278123156766}} 80000 + 20000
-
-# F3Net
-# {'davis': {'fmeasure': 0.8653015660248683, 'mae': 0.03404826561811981}} 0
-# {'davis': {'fmeasure': 0.8663272724268639, 'mae': 0.029167446537842773}} 120000
-# {'davis': {'fmeasure': 0.8704135170765226, 'mae': 0.029225478684095}} 100000
-# {'davis': {'fmeasure': 0.8707392987449726, 'mae': 0.028199705166410204}} 80000
-# {'davis': {'fmeasure': 0.8749002529630177, 'mae': 0.028620029588870388}} 60000
-# {'davis': {'fmeasure': 0.8841000759318469, 'mae': 0.02973330920502805}} 40000
-# {'davis': {'fmeasure': 0.8887817824566521, 'mae': 0.030533261610297624}} 20000
-# {'davis': {'fmeasure': 0.8890749113651485, 'mae': 0.02940475187227484}}
-# {'davis': {'fmeasure': 0.8756644270037889, 'mae': 0.027583954236518913}} no distillation
-
-# {'VOS': {'fmeasure': 0.7758744379092741, 'mae': 0.07942563745765158}} 20000
-# {'SegTrackV2': {'fmeasure': 0.8887886543479966, 'mae': 0.025292343551778392}} 20000
-# {'DAVSOD': {'fmeasure': 0.6443173444242073, 'mae': 0.08025684179355182}} 20000
-# {'DAVSOD': {'fmeasure': 0.6413284895557657, 'mae': 0.07660063219057435}} 60000
-
-# F3Net only finetune VideoSaliency_2020-09-01 07:15:39
-# {'davis': {'fmeasure': 0.877378962291034, 'mae': 0.026815478345757576}} 40000
-# {'davis': {'fmeasure': 0.8825429675996908, 'mae': 0.026620300446554314}} 30000
-# {'davis': {'fmeasure': 0.8785479143849427, 'mae': 0.026080884492927017}} 20000
-# {'davis': {'fmeasure': 0.8754949746438053, 'mae': 0.0278916474098535}} 10000
-
-# {'DAVSOD': {'fmeasure': 0.618976746648981, 'mae': 0.07551955168632846}} 10000
-# {'VOS': {'fmeasure': 0.7788612473435724, 'mae': 0.06646413065564855}}
-
-# F3Net only finetune VideoSaliency_2020-09-08 08:46:47
-# {'VOS': {'fmeasure': 0.7734146998329936, 'mae': 0.07729990310956811}} 2000
-# {'davis': {'fmeasure': 0.8656253041652912, 'mae': 0.0348368471552667}} 2000
-# {'DAVSOD': {'fmeasure': 0.6294509528059372, 'mae': 0.08523378453630998}} 2000
-
-# sequence training with l2 loss
-# {'davis': {'fmeasure': 0.8863832469146518, 'mae': 0.027627839811224686}}
-# {'davis': {'fmeasure': 0.8841633873367919, 'mae': 0.02688328257937753}}
-# {'davis': {'fmeasure': 0.8858619327344865, 'mae': 0.024967701169178107}}
-# {'davis': {'fmeasure': 0.8862040347235166, 'mae': 0.02505401840055184}}
-
-# gate module insert VideoSaliency_2020-08-22 14:17:51
-# {'davis': {'fmeasure': 0.889208981681908, 'mae': 0.025872335186497047}}
-# {'davis': {'fmeasure': 0.8893343580007665, 'mae': 0.02625041336905838}}
-# {'davis': {'fmeasure': 0.892129925183327, 'mae': 0.025956635846254534}} 30000
-
-# {'DAVSOD': {'fmeasure': 0.6241270723640747, 'mae': 0.07644525914911644}} 50000
-# {'DAVSOD': {'fmeasure': 0.6269053036187716, 'mae': 0.07612932474782834}} 30000
-
-# {'VOS': {'fmeasure': 0.781705699525925, 'mae': 0.07762718467830418}} 30000
-# {'SegTrackV2': {'fmeasure': 0.8769431698390447, 'mae': 0.02364640501186135}} 30000
-
-# R2Net
-# {'davis': {'fmeasure': 0.8560587150226311, 'mae': 0.02709158591258486}} 0
-# {'davis': {'fmeasure': 0.8740784146143851, 'mae': 0.030000806986006066}} 20000
-# {'davis': {'fmeasure': 0.8738029940264168, 'mae': 0.031259597996254655}} 40000
-# {'davis': {'fmeasure': 0.8683733244433338, 'mae': 0.032098539788818686}} 60000
-# {'davis': {'fmeasure': 0.8708696529215854, 'mae': 0.03109205073446645}} 80000
-# {'davis': {'fmeasure': 0.8651841128623684, 'mae': 0.02922830903432984}} 100000
-
-# {'DAVSOD': {'fmeasure': 0.6578043535898054, 'mae': 0.07776031258567824}} 80000
-# {'DAVSOD': {'fmeasure': 0.6583439205495094, 'mae': 0.07653715379416723}} 100000
-# {'VOS': {'fmeasure': 0.7760511417773258, 'mae': 0.07494954062830472}} 80000
-# {'VOS': {'fmeasure': 0.7723011074614098, 'mae': 0.07578435707489597}} 80000
-
-# R2Net only finetune
-# {'davis': {'fmeasure': 0.8548179629861553, 'mae': 0.030795675437143337}} 40000
-# {'davis': {'fmeasure': 0.8635315735308017, 'mae': 0.030016298981508035}} 30000
-# {'davis': {'fmeasure': 0.8625687113609614, 'mae': 0.028724036500521192}} 20000
-# {'davis': {'fmeasure': 0.8597927432756874, 'mae': 0.031393451661227506}} 10000
-
-# {'VOS': {'fmeasure': 0.734632638074282, 'mae': 0.0727141879076122}} 10000
-# {'VOS': {'fmeasure': 0.7261581810893859, 'mae': 0.07565893406248822}} 30000
-# {'DAVSOD': {'fmeasure': 0.6446839646609621, 'mae': 0.07180091274861225}} 30000
-
-# gate module insert
-# {'davis': {'fmeasure': 0.8748441653176292, 'mae': 0.027851314948555327}} 10000
-# {'davis': {'fmeasure': 0.873028833377723, 'mae': 0.028706954658253268}} 30000
-
-# {'VOS': {'fmeasure': 0.7570477998656433, 'mae': 0.07774102850737559}} 10000
-# {'DAVSOD': {'fmeasure': 0.662450524947881, 'mae': 0.07530461510457817}} 10000
-
-
-# RAS2
-# {'davis': {'fmeasure': 0.8229587839207361, 'mae': 0.042331282781968474}} 0 no distillation and no finetune
-# {'davis': {'fmeasure': 0.8757726533143092, 'mae': 0.031713322556592864}} 20000
-# {'davis': {'fmeasure': 0.877346289325937, 'mae': 0.0311400908295075}} 30000
-# {'davis': {'fmeasure': 0.8778071227148782, 'mae': 0.030914894533156396}} 40000
-# {'davis': {'fmeasure': 0.8789805277673451, 'mae': 0.03058252114962297}} 50000
-# {'davis': {'fmeasure': 0.8614372000730359, 'mae': 0.03390185397691291}} 100000
-# {'davis': {'fmeasure': 0.8653076651274525, 'mae': 0.03379165449002227}}
-
-# {'VOS': {'fmeasure': 0.7795804356591828, 'mae': 0.07915214435496876}} 20000
-
-# RAS2 only finetune
-# {'davis': {'fmeasure': 0.8605102182763539, 'mae': 0.03238801393782354}}
-# {'davis': {'fmeasure': 0.8566328414011626, 'mae': 0.033554104844020126}}
-
-# {'DAVSOD': {'fmeasure': 0.6138489186693313, 'mae': 0.07923908515233896}} 30000
-# {'VOS': {'fmeasure': 0.752770620576296, 'mae': 0.07330827505174432}} 30000
-
-# gate module insert
-# {'davis': {'fmeasure': 0.8825059020992001, 'mae': 0.030672534257460114}}
-# {'davis': {'fmeasure': 0.8829802090654366, 'mae': 0.030159273414776422}}
-
-# {'VOS': {'fmeasure': 0.7774251373208261, 'mae': 0.07819300471733173}} 20000
-# {'DAVSOD': {'fmeasure': 0.6414144085751449, 'mae': 0.07907701623820462}} 20000
-
-# F3Net soft loss weight:0.1
-# {'davis': {'fmeasure': 0.8826005704242365, 'mae': 0.02458922193742472}} 30000
-# {'davis': {'fmeasure': 0.8863944061775805, 'mae': 0.024060505036817413}} 20000
-# {'davis': {'fmeasure': 0.8804481307610312, 'mae': 0.025164723752833552}} 10000
-
-# F3Net soft loss weight:0.3
-# {'davis': {'fmeasure': 0.8839965558095962, 'mae': 0.02537649761741015}} 30000
-# {'davis': {'fmeasure': 0.887562597702381, 'mae': 0.024626356941501025}} 20000
-# {'davis': {'fmeasure': 0.8891568096857249, 'mae': 0.02591116499258871}} 10000
-
-# F3Net soft loss weight:0.7
-# {'davis': {'fmeasure': 0.8863653285893788, 'mae': 0.027940254455130632}} 30000
-# {'davis': {'fmeasure': 0.8893278413504562, 'mae': 0.02743635778153236}} 20000
-
-# F3Net soft loss weight:0.9
-# {'davis': {'fmeasure': 0.8920714411828287, 'mae': 0.027868697591622475}} 20000
-# {'DAVSOD': {'fmeasure': 0.6618834458323002, 'mae': 0.07729873498680018}}
-# {'VOS': {'fmeasure': 0.7989555033147239, 'mae': 0.07746995103929408}}
--- utils_mine.py
+++ utils_mine.py
@@ -1,38 +1,27 @@
 import torch
-from models.net import SNet
 import numpy as np
 from matplotlib import pyplot as plt
+import os
 
-import sys
-sys.path.append('/home/tangyi/code/SVS')
-plt.style.use('classic')
+class AvgMeter(object):
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
 
 def MaxMinNormalization(x,Max,Min):
     x = (x - Min) / (Max - Min)
     return x
-
-def fuse_MGA_F3Net(mga_model_path, f3net_path, net, device_id=0):
-    # net = SNet(cfg=None).cuda()
-    f3_model = torch.load(f3net_path, map_location='cuda:' + str(device_id))
-    mga_model = torch.load(mga_model_path, map_location='cuda:' + str(device_id))
-    mga_keys = list(mga_model.keys())
-    flow_keys = [key for key in mga_keys if key.find('resnet_aspp.backbone_features') > -1]
-    m_dict = net.state_dict()
-    for k in m_dict.keys():
-        if k in f3_model.keys():
-            print('loading F3Net key:', k)
-            param = f3_model.get(k)
-            m_dict[k].data = param
-        elif k.find('flow_bkbone') > -1:
-            print('loading MGA key:', k)
-            k_tmp = k.replace('flow_bkbone', 'resnet_aspp.backbone_features')
-            # k_tmp.replace('flow_bkbone', 'resnet_aspp.backbone_features')
-            m_dict[k].data = mga_model.get(k_tmp)
-        else:
-            print('not loading key:', k)
-
-    net.load_state_dict(m_dict)
-    return net
 
 
 def load_MGA(mga_model, model_path, device_id=0):
@@ -67,58 +56,49 @@ def load_part_of_model(new_model, src_model_path, device_id=0):
     new_model.load_state_dict(m_dict)
     return new_model
 
-def load_part_of_model2(new_model, src_model_path, device_id=0):
-    src_model = torch.load(src_model_path, map_location='cuda:' + str(device_id))
-    m_dict = new_model.state_dict()
-    for k in src_model.keys():
-        print(k)
-        param = src_model.get(k)
-        k = k.replace('module.', '')
-        m_dict[k].data = param
+def check_mkdir(dir_name):
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
 
-    new_model.load_state_dict(m_dict)
-    return new_model
 
-def visualize(input, save_path):
-    input = input.data.cpu().numpy()
-    for i in range(input.shape[1]):
-        plt.subplot(8, 8, i + 1)
-        plt.imshow(input[0, i, :, :])
+def cal_precision_recall_mae(prediction, gt):
+    # input should be np array with data type uint8
+    assert prediction.dtype == np.uint8
+    assert gt.dtype == np.uint8
+    assert prediction.shape == gt.shape
 
-    plt.savefig(save_path)
+    eps = 1e-4
 
-def visualize_vec(input_vec, save_path):
-    input = input_vec.data.cpu().numpy()
-    input = np.squeeze(input)
-    input = np.tile(input, (64, 1))
-    plt.imshow(input)
-    plt.colorbar()
-    plt.savefig(save_path)
+    prediction = prediction / 255.
+    gt = gt / 255.
 
-if __name__ == '__main__':
-    ckpt_path = './ckpt'
-    exp_name = 'VideoSaliency_2019-05-14 17:13:16'
+    mae = np.mean(np.abs(prediction - gt))
 
-    args = {
-        'snapshot': '30000',  # your snapshot filename (exclude extension name)
-        'crf_refine': False,  # whether to use crf to refine results
-        'save_results': True,  # whether to save the resulting masks
-        'input_size': (473, 473)
-    }
-    a = torch.rand([1, 64, 1, 1])
-    # visualize_vec(a, 'a.png')
-    # from MGA.mga_model import MGA_Network
-    # a = MGA_Network(nInputChannels=3, n_classes=1, os=16,
-    #             img_backbone_type='resnet101', flow_backbone_type='resnet34')
-    # load_MGA(a, '../pre-trained/MGA_trained.pth')
+    hard_gt = np.zeros(prediction.shape)
+    hard_gt[gt > 0.5] = 1
+    t = np.sum(hard_gt)
 
-    net = SNet(cfg=None).cuda()
-    net = fuse_MGA_F3Net('../pre-trained/MGA_trained.pth', '../pre-trained/F3Net.pth', net)
-    torch.save(net.state_dict(), '../pre-trained/SNet.pth')
-    # net = load_part_of_model(net, '../pre-trained/SNet.pth')
-    # input = torch.zeros([2, 3, 380, 380]).cuda()
-    # output = net(input, input)
-    # print(output[0].size())
-    # src_model_path = os.path.join(ckpt_path, exp_name, args['snapshot'] + '.pth')
-    # net = R3Net(motion='GRU')
-    # net = load_part_of_model(net, src_model_path)
+    precision, recall = [], []
+    # calculating precision and recall at 255 different binarizing thresholds
+    for threshold in range(256):
+        threshold = threshold / 255.
+
+        hard_prediction = np.zeros(prediction.shape)
+        hard_prediction[prediction > threshold] = 1
+
+        tp = np.sum(hard_prediction * hard_gt)
+        p = np.sum(hard_prediction)
+
+        precision.append((tp + eps) / (p + eps))
+        recall.append((tp + eps) / (t + eps))
+
+    return precision, recall, mae
+
+
+def cal_fmeasure(precision, recall):
+    assert len(precision) == 256
+    assert len(recall) == 256
+    beta_square = 0.3
+    max_fmeasure = max([(1 + beta_square) * p * r / (beta_square * p + r) for p, r in zip(precision, recall)])
+
+    return max_fmeasure
