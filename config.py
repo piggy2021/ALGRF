7af4eddef9e1c8aab124ded14bbffcb1d2b44c92
piggy2021 <82191047+piggy2021@users.noreply.github.com>
2021-04-19 11:29:37 +0800

    Update config.py

--- config.py
+++ config.py
@@ -9,12 +9,6 @@ datasets_root = '/home/amax/data/ty'
 # For each dataset, I put images and masks together
 video_train_path = os.path.join(datasets_root, 'Pre-train')
 davis_path = os.path.join(datasets_root, 'davis')
-fbms_path = os.path.join(datasets_root, 'FBMS')
-mcl_path = os.path.join(datasets_root, 'MCL')
-uvsd_path = os.path.join(datasets_root, 'UVSD')
 visal_path = os.path.join(datasets_root, 'ViSal')
-vos_path = os.path.join(datasets_root, 'VOS')
-segtrack_path = os.path.join(datasets_root, 'SegTrack-V2')
 davsod_path = os.path.join(datasets_root, 'DAVSOD')
-video_seq_path = os.path.join(datasets_root, 'video_saliency/train_all')
-video_seq_gt_path = os.path.join(datasets_root, 'video_saliency/train_all_gt2_revised')
+
