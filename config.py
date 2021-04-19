f8a7fb42071a23d0102f0b8ef8d3e592099a8d8c
piggy2021 <82191047+piggy2021@users.noreply.github.com>
2021-04-19 11:25:06 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,12 +7,6 @@ datasets_root = '/home/amax/data/ty'
 # datasets_root = '/home/qub/data/saliency'
 
 # For each dataset, I put images and masks together
-msra10k_path = os.path.join(datasets_root, 'msra10k')
-ecssd_path = os.path.join(datasets_root, 'ecssd')
-hkuis_path = os.path.join(datasets_root, 'hkuis')
-pascals_path = os.path.join(datasets_root, 'pascals')
-dutomron_path = os.path.join(datasets_root, 'dutomron')
-sod_path = os.path.join(datasets_root, 'sod')
 video_train_path = os.path.join(datasets_root, 'Pre-train')
 davis_path = os.path.join(datasets_root, 'davis')
 fbms_path = os.path.join(datasets_root, 'FBMS')
